577a3d6043845a1a67e60c92dc65e1c5a1e3eb28
jon <phompasit2323@gmail.com>
2025-03-02 23:15:01 +0700

    y

--- src/index.js
+++ src/index.js
@@ -5,18 +5,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import { SpeedInsights } from "@vercel/speed-insights/react"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
       <Suspense fallback={<div>Loading...</div>}>
+    <BrowserRouter>
         <ChakraProvider>
           <App />
-          <SpeedInsignts/>
         </ChakraProvider>
-      </Suspense>
     </BrowserRouter>
+      </Suspense>
   </React.StrictMode>
 );
 
